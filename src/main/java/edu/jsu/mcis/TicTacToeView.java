5a5120d0e0ba90ae3ef6504780a93ee3400465f4
chrisholt29 <chrisholt29@gmail.com>
2018-02-14 20:25:54 -0600

    Update TicTacToeView.java

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -16,7 +16,20 @@ public class TicTacToeView {
         
         /* Print the board to the console (see examples) */
         
-        /* INSERT YOUR CODE HERE */
+        System.out.print("\n\n");
+        System.out.print("  ");
+        for (int p = 0; p < model.getWidth(); p++) {
+            System.out.print(p);
+        }
+        System.out.print("\n");
+        for (int i = 0; i < model.getWidth(); i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < model.getWidth(); j++){
+               System.out.print(model.getMark(i, j));
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n\n");
 
     }
 
@@ -24,7 +37,14 @@ public class TicTacToeView {
 
         /* Display a prompt for the player's next move (see examples) */
 
-        /* INSERT YOUR CODE HERE */
+        if (model.isXTurn()){
+            System.out.println("Player 1 (X) Move:\n" +
+            "Enter the row and column numbers, separated by a space: ");
+        }
+        else {
+            System.out.println("Player 2 (O) Move:\n" +
+            "Enter the row and column numbers, separated by a space: ");
+        }
 
     }
 
@@ -32,7 +52,7 @@ public class TicTacToeView {
 
         /* Display an error if input is invalid (see examples) */
 
-        /* INSERT YOUR CODE HERE */
+        System.out.println("Invalid Input");
 
     }
 
